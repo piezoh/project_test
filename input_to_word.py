0f09c0e60796fde770865d5a8d987f8c78647174
piezoh <noriko.piezoh@gmail.com>
2023-07-22 17:48:41 +0900

    HTML編集

--- input_to_word.py
+++ input_to_word.py
@@ -1,5 +1,7 @@
 from docx import Document
 from flask import Flask, request, render_template
+import tkinter.filedialog as filedialog
+from tkinter import Tk
 
 app = Flask(__name__)
 
@@ -13,20 +15,29 @@ def index():
 @app.route("/get_text", methods=["POST"])
 def get_text():
     # フォームから入力されたテキストを取得
+    doc_number = request.form["doc_number"]
+    doc_date = request.form["doc_date"]
+    draft_date = request.form["draft_date"]
+    drafter = request.form["drafter"]
+    summary = request.form["summary"]
+    authorizere = request.form["authorizere"]
     input_text = request.form["input_text"]
-    filename = request.form["filename"]
+
+    root = Tk()
+    root.withdraw()  # メインウィンドウを非表示にする
+    filepath = filedialog.asksaveasfilename(defaultextension=".docx", filetypes=[("Word Document", "*.docx")])
 
     # Word文書を生成して保存
-    generate_docx(filename, input_text)
+    generate_docx(filepath, input_text, doc_date, doc_number, draft_date, drafter, summary, authorizere)
 
     # 応答メッセージを返す
-    return f"ファイル '{filename}.docx' が保存されました。"
+    return "ファイルが保存されました。"
 
 
 # 全文を改行及び文字数で区切ってリストに入れる
 def slice_txt_into_list(full_txt, slice_length):
     sliced_list = []
-    lines = full_txt.split("\n")  # 改行でテキストを分割
+    lines = full_txt.split("\r\n")  # 改行でテキストを分割
 
     for line in lines:
         while len(line) > slice_length:
@@ -39,28 +50,63 @@ def slice_txt_into_list(full_txt, slice_length):
     return sliced_list
 
 
-def generate_docx(new_filename, full_txt):
-    # 40字ごとに区切ってリストに入れる
-    draft_content = slice_txt_into_list(full_txt, 40)
+def generate_docx(output_path, full_txt, date, doc_num, drft_date, drft_person, summary_content, author):
+    # 39字ごとに区切ってリストに入れる
+    draft_content = slice_txt_into_list(full_txt, 39)
 
-    # 既存のWord文書を開く
-    document = Document("TEST用起案文書様式.docx")
+    # 決済者によってWordテンプレートを選択して開く
+    if author == "町長":
+        document = Document("起案文書様式.docx")
+    elif author == "副町長":
+        document = Document("起案文書様式(副町長まで).docx")
+    else:
+        document = Document("起案文書様式(課長まで).docx")
 
     # 文書内の表を取得する
     table = document.tables[0]
 
-    # 起案文書様式の６行目から入力
+    # 文書番号欄のセルを取得
+    doc_num_cell = table.cell(1, 8)
+
+    # 文書番号欄の説に文書番号を入力
+    doc_num_cell.text = doc_num
+
+    # 文書の日付欄のセルを取得
+    date_cell = table.cell(2, 8)
+
+    # 文書の日付欄のセルに日付を入力
+    date_cell.text = date
+
+    # 起案日&起案者欄のセルを取得
+    draft_date_cell = table.cell(3, 2)
+
+    # 起案日と起案者を同じセルに入力するため連結
+    drafter_date = "起案  " + drft_date + "\n" + "\n" + drft_person
+
+    # 起案日&起案者を入力
+    draft_date_cell.text = drafter_date
+
+    # 摘要欄のセルを取得
+    summary_content_cell = table.cell(4, 4)
+
+    # 摘要欄に内容を入力
+    summary_content_title = "摘要\n" + summary_content
+    summary_content_cell.text = summary_content_title
+
+    # 本文を起案文書様式の６行目から入力
     line_num = 6
 
     for draft in draft_content:
         # 表のline_num行目、0列目にあるセルを取得する
         cell = table.cell(line_num, 0)
+
+        for paragraph in cell.paragraphs:
+            paragraph.text = paragraph.text.replace("\r", "")
         # セル内のテキストを上書きする
         cell.text = draft
         line_num += 1
 
-    # 上書きした文書を保存
-    output_path = f"{new_filename}.docx"
+    # 上書きした文書を保存、保存場所指定
     document.save(output_path)
 
 
