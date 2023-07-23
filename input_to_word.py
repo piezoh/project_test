029770c25e925ec014c17c9bbcfebaeefaf5c309
piezoh <noriko.piezoh@gmail.com>
2023-07-23 12:53:41 +0900

    日付完成

--- input_to_word.py
+++ input_to_word.py
@@ -1,7 +1,8 @@
 from docx import Document
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, current_app
 import tkinter.filedialog as filedialog
 from tkinter import Tk
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -16,8 +17,10 @@ def index():
 def get_text():
     # フォームから入力されたテキストを取得
     doc_number = request.form["doc_number"]
-    doc_date = request.form["doc_date"]
-    draft_date = request.form["draft_date"]
+    doc_date_str = request.form["doc_date"]
+    doc_date = datetime.strptime(doc_date_str, "%Y-%m-%d")
+    draft_date_str = request.form["draft_date"]
+    draft_date = datetime.strptime(draft_date_str, "%Y-%m-%d")
     drafter = request.form["drafter"]
     summary = request.form["summary"]
     authorizere = request.form["authorizere"]
@@ -28,10 +31,21 @@ def get_text():
     filepath = filedialog.asksaveasfilename(defaultextension=".docx", filetypes=[("Word Document", "*.docx")])
 
     # Word文書を生成して保存
-    generate_docx(filepath, input_text, doc_date, doc_number, draft_date, drafter, summary, authorizere)
+    with current_app.app_context():
+        generate_docx(filepath, input_text, doc_date, doc_number, draft_date, drafter, summary, authorizere)
 
     # 応答メッセージを返す
     return "ファイルが保存されました。"
+
+
+def convert_to_fullwidth_numbers(text):
+    # 半角数字と対応する全角数字の辞書を定義
+    halfwidth_numbers = "0123456789"
+    fullwidth_numbers = "０１２３４５６７８９"
+    num_conversion = str.maketrans(halfwidth_numbers, fullwidth_numbers)
+
+    # 変換を行って返す
+    return text.translate(num_conversion)
 
 
 # 全文を改行及び文字数で区切ってリストに入れる
@@ -74,14 +88,35 @@ def generate_docx(output_path, full_txt, date, doc_num, drft_date, drft_person, 
     # 文書の日付欄のセルを取得
     date_cell = table.cell(2, 8)
 
+    # 文書の日付を和暦に変換＆数字を全角に変換
+    doc_year_int = int(date.strftime("%Y"))
+
+    doc_year = convert_to_fullwidth_numbers(str(doc_year_int - 2018))
+    doc_month = convert_to_fullwidth_numbers(date.strftime("%m"))
+    doc_day = convert_to_fullwidth_numbers(date.strftime("%d"))
+
+    # 文書の日付を和暦に変換
+    jpn_doc_date = f"令和{doc_year}年{doc_month}月{doc_day}日"
+
     # 文書の日付欄のセルに日付を入力
-    date_cell.text = date
+    date_cell.text = jpn_doc_date
 
     # 起案日&起案者欄のセルを取得
     draft_date_cell = table.cell(3, 2)
 
+    # チェック
+    # 起案の日付を和暦に変換＆数字を全角に変換
+    drft_year_int = int(drft_date.strftime("%Y"))
+
+    drft_year = convert_to_fullwidth_numbers(str(drft_year_int - 2018))
+    drft_month = convert_to_fullwidth_numbers(drft_date.strftime("%m"))
+    drft_day = convert_to_fullwidth_numbers(drft_date.strftime("%d"))
+
+    # 文書の日付を和暦に変換
+    jpn_drft_date = f"令和{drft_year}年{drft_month}月{drft_day}日"
+
     # 起案日と起案者を同じセルに入力するため連結
-    drafter_date = "起案  " + drft_date + "\n" + "\n" + drft_person
+    drafter_date = "起案  " + jpn_drft_date + "\n" + "\n" + drft_person
 
     # 起案日&起案者を入力
     draft_date_cell.text = drafter_date
